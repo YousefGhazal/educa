4e5c4910c2558174b04efdca01f1023788664072
Yousef <yousefghazal2412@gmail.com>
2023-02-07 14:13:07 +0200

    adding permissions to routers

--- educa/urls.py
+++ educa/urls.py
@@ -21,13 +21,14 @@ from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('admin/', admin.site.urls),
-    path('course/', include('courses.urls')),
-    path('', CourseListView.as_view(), name='course_list'),
-    path('students/', include('students.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("admin/", admin.site.urls),
+    path("course/", include("courses.urls")),
+    path("", CourseListView.as_view(), name="course_list"),
+    path("students/", include("students.urls")),
+    path("api/", include("courses.api.urls"), name="api"),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
